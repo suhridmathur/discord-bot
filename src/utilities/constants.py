accc1df58fbec5d3df3da41c64daf4d7f53eef55
Suhrid Mathur <suhrid.mathur@industrybuying.com>
2020-09-17 02:46:18 +0530

    Add GoogleSearch Service - Added GoogleSearch class which can 	- Search using Google's API the given searched term 	- Cache the results
    - Added GoogleSearch Cache-Timeout in constants.py

--- src/utilities/constants.py
+++ src/utilities/constants.py
@@ -1,3 +1,10 @@
+import datetime
+
+# MESSAGES
 USER_GREETING_MESSAGES = ("hey", "hi", "good morning", "good evening", "hello", "hii", "heyy")
 
 BOT_GREETING_MESSAGES = ("Hey", "Hi", "Whats up", "How are you doing")
+
+
+# Google Search
+GOOGLE_SEARCH_CACHE_TIMEOUT = datetime.timedelta(hours=6)
