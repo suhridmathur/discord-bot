fdd67ed122e30afc5e4ac47ff0d206124df27f47
Suhrid Mathur <suhrid.mathur@industrybuying.com>
2020-09-17 02:50:28 +0530

    Handle Response For Google Search - Using GoogleSearch class to search terms on google when receiving input of format `!google search-term`. - Returning Embeded Response with Urls.

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@ import random
 
 import discord
 
-from utilities import constants
+from utilities import constants, google_search
 
 TOKEN = os.getenv("BOT_TOKEN")
 
@@ -14,12 +14,36 @@ class Client(discord.Client):
         if self.user == message.author:
             return
 
-        # Send Hello message on greeting from user #
         message_content = message.content
+
+        # Send Hello message on greeting from user #
         if message_content.lower() in constants.USER_GREETING_MESSAGES:
             await message.channel.send(
                 f"{random.choice(constants.BOT_GREETING_MESSAGES)} {message.author.display_name}"
             )
+
+        # Search on Google if Message StartsWith "!google" #
+        elif message_content.startswith("!google"):
+            search_term = " ".join(
+                message_content.split()[1:]
+            )  # `!google leo messi` -> `leo messi`#
+            if not search_term:
+                await message.channel.send(
+                    random.choice(constants.NO_TERM_ENTERED_MESSAGES)
+                )
+                return
+
+            search_service = google_search.GoogleSearch()
+            results = search_service.get_top_five_links(search_term)
+
+            embeded_response = discord.Embed(
+                title=f"{constants.EMBEDDED_RESULTS_TITLE} '{search_term}'",
+            )
+            for result in results:
+                embeded_response.add_field(
+                    name=result["title"], value=result["url"], inline=False
+                )
+            await message.channel.send(embed=embeded_response)
 
 
 client = Client()
